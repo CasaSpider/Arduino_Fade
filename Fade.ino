cf09b3dad1d4f988eb297be07353c68cfe448122
CasaSpider <peter.nijhuis@brabantwater.nl>
2019-07-31 20:16:31 +0200

    Testing from Code

--- Fade.ino
+++ Fade.ino
@@ -20,6 +20,7 @@ int brightness = 0; // how bright the LED is
 int fadeAmount = 5; // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
+// test...
 void setup() {
   // declare pin 9 to be an output:
   pinMode(ledR, OUTPUT);
